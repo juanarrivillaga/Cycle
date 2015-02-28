79ab6448ad63f31f45b98eef47328eb222c78962
juanarrivillaga <juanpa.arrivillaga@gmail.com>
2015-02-28 03:31:59 -0500

    added some comments to cycle driver

--- src/CycleDriver.java
+++ src/CycleDriver.java
@@ -1,8 +1,8 @@
 
 public class CycleDriver {
 	public static void main(String[] args){
-		Cycle c1 = new Cycle();
-		System.out.print(c1.toString());
+		Cycle c1 = new Cycle(); // create cycle object using default constructor
+		System.out.print(c1.toString());//display object properties by calling toString method
 	}
 }
 /* SAMPLE OUTPUT:
